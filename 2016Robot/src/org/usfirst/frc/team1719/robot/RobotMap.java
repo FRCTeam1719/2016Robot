85f765285164c4a1df2b2d5f3a0225bbdc0b2a3e
aaron <aeline@parkschool.net>
2016-02-14 13:37:46 -0500

    Updated RobotMap
    Cleaned Up
    Updated PortMapping documentation, posted to #electrical & #programming

--- 2016Robot/src/org/usfirst/frc/team1719/robot/RobotMap.java
+++ 2016Robot/src/org/usfirst/frc/team1719/robot/RobotMap.java
@@ -25,11 +25,10 @@ public class RobotMap {
     // public static int rangefinderModule = 1;
 	static double FLYWHEEL_CIRCUMFRENCE_FEET = 1.57075;
 	
-	static Spark leftDriveController;
-	static Spark rightDriveController;
-
-
-
+	public static Spark leftDriveController;
+	public static Spark rightDriveController;
+	public static Encoder leftDriveEncoder;
+	public static Encoder rightDriveEncoder;
 	public static Spark rightFlyWheelController;
 	public static Encoder rightFlyWheelEncoder;
 	public static Spark leftFlyWheelController;
@@ -48,8 +47,8 @@ public class RobotMap {
 		//Main hardware allocation
 		
 		//Motor Controllers
-		leftDriveController = new Spark(1);    
 		rightDriveController = new Spark(0);
+		leftDriveController = new Spark(1);    
 		leftFlyWheelController = new Spark(2);
 		rightFlyWheelController = new Spark(3);
 		armController = new Spark(4);
@@ -63,13 +62,20 @@ public class RobotMap {
 		configureEncoder(rightFlyWheelEncoder);
 		leftFlyWheelEncoder = new Encoder(4, 5, true, Encoder.EncodingType.k4X);
 		configureEncoder(leftFlyWheelEncoder);
+		rightDriveEncoder = new Encoder(6, 7, true, Encoder.EncodingType.k4X);
+		configureEncoder(rightDriveEncoder);
+		leftDriveEncoder = new Encoder(8, 9, true, Encoder.EncodingType.k4X);
+		configureEncoder(leftDriveEncoder);
+        buttonA = new DigitalInput(19);
+		buttonB = new DigitalInput(20);
 		
 		//Analog In
-		dial = new AnalogInput(3);
-		//gyro = new AnalogGyro(2);
+		gyro = new AnalogGyro(0);
         armPot = new AnalogPotentiometer(1, 1200.0D);
-        buttonA = new DigitalInput(9);
-		buttonB = new DigitalInput(10);
+		dial = new AnalogInput(3);
+
+
+
 	}
 	
 	/**
