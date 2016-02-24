a20da93a7898259b8ac80cfdf3499c3c0beea481
Quintin <quintinf19@parkschool.net>
2016-02-23 19:09:56 -0500

    Added things to Commit

---
+++ 2016Robot/src/org/usfirst/frc/team1719/robot/commands/EngagePhotonCannon.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team1719.robot.commands;
+
+import org.usfirst.frc.team1719.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class EngagePhotonCannon extends Command {
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		if(Robot.arm.getArmAngle() < 60){
+			Robot.photonCannon.isOn = !Robot.photonCannon.isOn;
+			if(Robot.photonCannon.isOn == true){
+				Robot.photonCannon.turnOn();
+			}else{
+				Robot.photonCannon.turnOff();
+			}
+		}
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
